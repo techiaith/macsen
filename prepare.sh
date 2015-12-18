f10191bc501c5426ed0519535777602d33aff9ac
Uned Technolegau Iaith <techiaith@bangor.ac.uk>
2015-12-18 15:40:13 +0000

    cywiriadau bach

--- prepare.sh
+++ prepare.sh
@@ -3,11 +3,11 @@ sudo apt-get update
 
 sudo easy_install --upgrade pip
 sudo pip install --upgrade setuptools
-sudo pip install -r jasper/client/requirements.txt
+sudo pip install -r client/requirements.txt
 
 sudo apt-get install festival festival-dev
 sudo mkdir -p /usr/share/festival/voices/welsh
-wget --progress=dot:mega -O -https://github.com/PorthTechnolegauIaith/llais_festival/archive/v1.0.tar.gz | tar -zxf - -C /usr/share/festival/voices/welsh
+wget --progress=dot:mega -O - https://github.com/PorthTechnolegauIaith/llais_festival/archive/v1.0.tar.gz | tar -zxf - -C /usr/share/festival/voices/welsh
 
 sudo apt-get install build-essential zlib1g-dev flex libasound2-dev libesd0-dev libsndfile1-dev
 
