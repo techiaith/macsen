ab8bc7b70f4e3cde0e700c326f07421ffe9870f1
Uned Technolegau Iaith <techiaith@bangor.ac.uk>
2015-12-21 11:10:48 +0000

    fersiwn sy'n gweithio...

--- prepare.sh
+++ prepare.sh
@@ -7,12 +7,22 @@ sudo pip install -r client/requirements.txt
 
 sudo apt-get install festival festival-dev
 sudo mkdir -p /usr/share/festival/voices/welsh
-wget --progress=dot:mega -O - https://github.com/PorthTechnolegauIaith/llais_festival/archive/v1.0.tar.gz | tar -zxf - -C /usr/share/festival/voices/welsh
+cd /usr/share/festival/voices/welsh
+wget --progress=dot:mega -O - https://github.com/PorthTechnolegauIaith/llais_festival/archive/v1.0.tar.gz | sudo tar -zxf -
+sudo mv llais_festival-1.0/* .
+sudo rmdir llais_festival-1.0
+cd -
 
-sudo apt-get install build-essential zlib1g-dev flex libasound2-dev libesd0-dev libsndfile1-dev
+mkdir -p $HOME/src/
 
-wget http://sourceforge.jp/projects/julius/downloads/60273/julius-4.3.1.tar.gz | tar -zxf - -C ~/
-cd ~/julius
+cd $HOME/src
+sudo apt-get install -y cvs build-essential zlib1g-dev flex libasound2-dev libesd0-dev libsndfile1-dev
+cvs -z3 -d:pserver:anonymous@cvs.sourceforge.jp:/cvsroot/julius co julius4
+#export CFLAGS="-O2 -mcpu=arm1176jzf-s -mfpu=vfp -mfloat-abi=hard -pipe -fomit-frame-pointer"
+cd -
+
+cd $HOME/src/julius4
 ./configure --enable-words-int
 make
 sudo make install
+cd -
