6819c1506f0765ba0d7dfeb4b4c3f2e546355671
techiaith <techiaith@bangor.ac.uk>
2016-01-05 11:20:45 +0000

    dileu xgettext

--- l10n/xgettext.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-if [ ! -f Macsen.po ]; then
-	echo "" > Macsen.po
-fi
-
-find . -path ./client/modules -prune -o -iname "*.py" -print | xargs xgettext -j --from-code=UTF-8 --default-domain=Macsen
-sed --in-place Macsen.po --expression=s/CHARSET/UTF-8/
