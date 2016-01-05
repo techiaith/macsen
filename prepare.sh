b42dac0881c3de29dbbb091e59877aff34ba69b4
techiaith <techiaith@bangor.ac.uk>
2016-01-05 11:55:09 +0000

    festival 1.1.0

--- prepare.sh
+++ prepare.sh
@@ -8,9 +8,9 @@ sudo pip install -r client/requirements.txt
 sudo apt-get install festival festival-dev
 sudo mkdir -p /usr/share/festival/voices/welsh
 cd /usr/share/festival/voices/welsh
-wget --progress=dot:mega -O - https://github.com/PorthTechnolegauIaith/llais_festival/archive/v1.0.tar.gz | sudo tar -zxf -
-sudo mv llais_festival-1.0/* .
-sudo rmdir llais_festival-1.0
+wget --progress=dot:mega -O - https://github.com/PorthTechnolegauIaith/llais_festival/archive/v1.1.0.tar.gz | sudo tar -zxf -
+sudo mv llais_festival-1.1.0/* .
+sudo rmdir llais_festival-1.1.0
 cd -
 
 mkdir -p $HOME/src/
